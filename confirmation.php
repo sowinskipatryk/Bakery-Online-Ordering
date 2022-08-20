2ef093c81329690db546601bfeb0cf7813f8c671
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-08-20 20:41:14 +0300

    styling

--- confirmation.php
+++ confirmation.php
@@ -2,14 +2,17 @@
 <html lang="pl">
 <head>
 	<meta charset="utf-8" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 	<title>Piekarnia Fredek - Potwierdzenie zamówienia</title>
 </head>
 <body>
+<div style="padding: 20px 0px 0px 20px;">
 <?php
 	
 	echo "<h2>Twoje zamówienie zostało przyjęte. Dziękujemy!</h2>"
 
 ?>
 
+</div>
 </body>
 </html>
